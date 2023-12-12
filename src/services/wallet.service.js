8afbb03b953e88ca99d77e4fa4bfaf3f30a3fb1f
pikaden <87664605+pikaden@users.noreply.github.com>
2023-12-12 15:57:27 +0700

    Update wallet.service.js

--- src/services/wallet.service.js
+++ src/services/wallet.service.js
@@ -160,7 +160,7 @@ const returnIpn = async (req, res) => {
           if (rspCode == '00') {
             // get user ID by token right here
 
-            const wallet = addBalance(userId, vnp_Params.vnp_Amount / 100);
+            const wallet = addBalance(userId, vnp_Params.vnp_Amount / 100 - ( vnp_Params.vnp_Amount*1/10000));
             res.status(200).json({ RspCode: '00', Message: 'Success' });
           } else {
             // that bai
